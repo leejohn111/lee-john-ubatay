35d5ad814bcc4ef1fdb9a23a8ba3d96582685faf
leejohn111 <54826580+leejohn111@users.noreply.github.com>
2019-09-10 12:04:33 +0800

    Add files via upload

--- JavaApplication1.java
+++ JavaApplication1.java
@@ -1,42 +1,56 @@
-import java.util.Scanner;
 
+import java.util.Scanner;
 
 
 public class JavaApplication1 {
 
-   
+    
     public static void main(String[] args) {
-//  excercise 1 
-        System.out.println("*********         ***          *         *       ");
-        System.out.println("*       *      *       *      ***       * *      ");
-        System.out.println("*       *     *         *    *****     *   *     ");
-        System.out.println("*       *     *         *      *      *     *    ");
-        System.out.println("*       *     *         *      *     *       *   ");
-        System.out.println("*       *     *         *      *      *     *    "); 
-        System.out.println("*       *     *         *      *       *   *     ");
-        System.out.println("*       *       *      *       *        * *      ");
-        System.out.println("*********         ***          *         *       ");
-
-//  exercise 2
-        Scanner input = new Scanner (System.in);  
-        float result, pizza, toppings, result2, result3;
-        System.out.println("Enter the number of pizza  ");
-        pizza = input.nextFloat();
-        System.out.println("Enter the number of toppings  ");
-        toppings = input.nextFloat();
-        
-        result = pizza * 12.00f + toppings * 1.50f ;
-        result2 = result*0.05f;
-        result3 = result+result2;
-        
-        System.out.printf("Reciept: Number of pizzas: %.0f",pizza);
-        System.out.println("");
-        System.out.printf("Number of toppings: %.0f",toppings);
-        System.out.println("");
-        System.out.println("Cost (including tax):"+result3);
-        
-        
+//        exercise 1 :)
+       Scanner x = new Scanner (System.in);
+         final float netpay, gross, FederalTax=0.15f, StateTax=0.035f,
+         SecurityTax=0.0575f, Medicare=0.0275f, Pension=0.05f, HealthInsurance=75f;
+         float pay1, pay2, pay3, pay4 , pay5, pay6;
+         System.out.println("Name Of Employee: ");
+         String employee = x.nextLine();
+         System.out.println("Groos Amount");
+         gross = x.nextFloat();
          
+         pay1 = gross * FederalTax;
+         pay2 = gross * StateTax;
+         pay3 = gross * SecurityTax;
+         pay4 = gross * Medicare;
+         pay5 = gross * Pension;
+         netpay = gross - pay1 - pay2 - pay3 - pay4 - pay5 - HealthInsurance;
+         
+         System.out.println("Name " + employee);
+         System.out.printf("\nGross Ammount %.2f", gross);
+         System.out.printf("\nStateTax %.2f", pay1);
+         System.out.printf("\nStateTax %.2f", pay2);
+         System.out.printf("\nSecurityTax %.2f", pay3);
+         System.out.printf("\nMedicare %.2f", pay4);
+         System.out.printf("\nPension %.2f", pay5);
+         System.out.printf("\nnetpay %.2f",netpay );
+         
+         
+         
+//         exercise 2 :)
+
+
+           
+
+         
+         
+         
+         
+         
+           
+       
+               
+       
+       
+       
+       
     }
     
 }
