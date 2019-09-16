b9e4d55cef1eb5937f9abede57dfe6191afa3dae
leejohn111 <54826580+leejohn111@users.noreply.github.com>
2019-09-16 11:55:36 +0800

    Add files via upload

--- JavaApplication5.java
+++ JavaApplication5.java
@@ -9,22 +9,86 @@ public class JavaApplication5 {
     
     public static void main(String[] args) {
         Scanner x = new Scanner(System.in);
-        Random random = new Random();
-        int number1 = random.nextInt(10);
-        int number2 = random.nextInt(10);
+        
+      int num1 = x.nextInt();
+      int num2 = x.nextInt();
+      
+    
+      
+      if (num1>num2)
+      {
+          System.out.println("The 1st number is less than the 2nd number, please multiply");
+                   System.out.println("The Product is " + num1*num2);
+      } 
+        else if (num1>num2)
+               {
+                System.out.println("The 1st number is greater than the 2nd number, please divide");
+                System.out.println("The qoutient is " + num1/num2 );
+                System.out.println("the qoutient is " + num1%num2 );
+                
+               } else 
+                System.out.println("The two numbers are equal, please add");
+                   System.out.println("The sum is " + (num1+num2));
+                
+                }
         
         
-        int num1 = (int)Math.random()*10;
-        int num2 = (int)Math.random()*10;
         
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+                                                      
 
-           System.out.println("Wat is the sum of" + number1 +" + " + number2 +" ? ");
-           int studentanswer = x.nextInt();
-           int sum = number1 + number2;
-           System.out.println(studentanswer == sum);
-           
-           if(studentanswer == sum);
-            System.out.println("correct");
+
+
+//        int num = x.nextInt();
+//        if(num%5 == 0)System.out.println("Hifive");
+//        if(num%2 == 0)System.out.println("HiEven");
+//        
+//        else
+//            System.out.println("you are wrong!!!");
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+//        Random random = new Random();
+//        int number1 = random.nextInt(10);
+//        int number2 = random.nextInt(10);
+//        
+//        
+//        int num1 = (int)Math.random()*10;
+//        int num2 = (int)Math.random()*10;
+//        
+//
+//           System.out.println("Wat is the sum of" + number1 +" + " + number2 +" ? ");
+//           int studentanswer = x.nextInt();
+//           int sum = number1 + number2;
+//           System.out.println(studentanswer == sum);
+//           
+//           if(studentanswer == sum);
+//            System.out.println("correct");
            
            
            
