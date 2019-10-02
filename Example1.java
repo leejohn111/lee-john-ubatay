7bfa72bcb1af781a336b5a80b5620dcea872aa3d
leejohn111 <54826580+leejohn111@users.noreply.github.com>
2019-10-02 11:28:23 +0800

    Add files via upload

---
+++ Example1.java
@@ -0,0 +1,59 @@
+
+package example1;
+
+import java.util.Scanner;
+
+public class Example1 {
+
+    
+    public static void main(String[] args) {
+     Scanner input = new Scanner(System.in);
+        
+        
+        
+        String message = "Welcome to java";
+        String message2 = message + " any additional message";
+        System.out.println(message.length());
+        System.out.println(message.charAt(14));
+        System.out.println(message.concat(" for dummies!"));
+        System.out.println(message.toUpperCase());
+        System.out.println(message.toLowerCase());
+        System.out.println(message.trim());
+        System.out.println(message + " for beginners ");
+        
+        
+        
+        
+//        char x = 92;
+//        System.out.println(x);
+//        char var = 'a';
+//        System.out.println(Character.isLetter(var));
+//        System.out.println(Character.isDigit(var));
+//        System.out.println(Character.isLowerCase(var));
+//        System.out.println(Character.isLetterOrDigit(var));
+//        System.out.println(Character.isUpperCase(var));
+//        System.out.println(Character.toUpperCase(var));
+//        System.out.println(Character.toLowerCase(var));
+//        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+//        int i = 'a';
+//            char c = 97;
+        
+        
+//        System.out.println(Math.cos(25));
+        
+        
+        
+    }
+    
+}
